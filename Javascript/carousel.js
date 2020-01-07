48d20355b3ae5da0cd3f7b49b20a82a9afb73a44
Desi Curry <Blackice@Desis-MacBook-Air.local>
2020-01-07 10:09:43 -0500

    updated all files waiting for frontend links

---
+++ Javascript/carousel.js
@@ -0,0 +1,36 @@
+class Carousel {
+    constructor(element) {
+      this.element = element;
+      this.imagesSlide = document.querySelector(".carousel-images");
+      this.imagesOne = document.querySelectorAll(".carousel-img");
+      this.imagesOneSingle = document.querySelector(".carousel-img");
+      this.currentIndex = 0;
+  
+      this.change();
+    }
+    change() {
+      const images = document.querySelectorAll(".carousel-img");
+      const newImagesArray = [];
+      Array.from(images).map(image => {
+        image.style.display = "none";
+        newImagesArray.push(image);
+        return this.currentIndex;
+      });
+  
+      this.timerStart();
+  
+      if (this.currentIndex == newImagesArray.length) {
+        this.currentIndex = 0;
+      }
+  
+      newImagesArray[this.currentIndex].style.display = "flex";
+      this.currentIndex += 1;
+    }
+  
+    timerStart() {
+      setTimeout(() => this.change(this.currentIndex), 5000);
+    }
+  }
+  
+  let carousel = document.querySelectorAll(".carousel-img").forEach(img => new Carousel(img));
+  
---
+++ Javascript/meetUs.js
@@ -0,0 +1,85 @@
+class Person {
+    constructor(element) {
+      this.element = element;
+      this.openButton = element.querySelector(".panel-button-open");
+      this.panelBar = element.querySelector(".panel-bar");
+      this.panel = element.querySelector(".panel");
+      this.closeButton = element.querySelector(".panel-button-close");
+      this.panelContent = element.querySelector(".panel-content");
+      this.showContent = element.querySelector(".showContent");
+      // this.footer = element.querySelector('.container-meetUs');
+  
+      this.openButton.addEventListener("click", () => {
+        this.panelOpen();
+        TweenMax.to(".panel", 1, { y: 100 });
+      });
+  
+      this.closeButton.addEventListener("click", () => {
+        this.panelOpen();
+        TweenMax.to(".panel", 1, { y: 0 });
+      });
+    }
+  
+    panelOpen() {
+      this.panelContent.classList.toggle("showContent");
+      this.closeButton.classList.toggle("hide-button");
+      this.openButton.classList.toggle("hide-button");
+    }
+  }
+  
+  const together = document.querySelectorAll(".panel");
+  together.forEach(panel => new Person(panel));
+  
+  ////////////////////////////////Events
+  
+  ///Linkedin hover////
+  let linkedinIcon = document.querySelectorAll(".linkedin-icon");
+  
+  Array.from(linkedinIcon).map(icon => {
+    icon.addEventListener("mouseover", event => {
+      icon.src = "https://img.icons8.com/nolan/90/000000/linkedin.png";
+    });
+  
+    icon.addEventListener("mouseleave", event => {
+      icon.src =
+        "https://img.icons8.com/material-outlined/90/000000/linkedin.png";
+    });
+  });
+  
+  ///Github hover////
+  let githubIcon = document.querySelectorAll(".github-icon");
+  
+  Array.from(githubIcon).map(icon => {
+    icon.addEventListener("mouseover", event => {
+      icon.src = "https://img.icons8.com/nolan/90/000000/github.png";
+    });
+  
+    icon.addEventListener("mouseleave", event => {
+      icon.src = "https://img.icons8.com/ios-glyphs/90/000000/github.png";
+    });
+  });
+  ///Down Arrow hover////
+  let downIcon = document.querySelectorAll(".panel-button-open");
+  
+  Array.from(downIcon).map(icon => {
+    icon.addEventListener("mouseover", event => {
+      icon.src = "https://img.icons8.com/nolan/64/000000/down.png";
+    });
+  
+    icon.addEventListener("mouseleave", event => {
+      icon.src = "https://img.icons8.com/ios-glyphs/50/000000/down.png";
+    });
+  });
+  
+  ///Up Arrow Hover///
+  let upIcon = document.querySelectorAll(".panel-button-close");
+  
+  Array.from(upIcon).map(icon => {
+    icon.addEventListener("mouseover", event => {
+      icon.src = "https://img.icons8.com/nolan/64/000000/left-up2.png";
+    });
+  
+    icon.addEventListener("mouseleave", event => {
+      icon.src = "https://img.icons8.com/ios/64/000000/left-up2-filled.png";
+    });
+  });
